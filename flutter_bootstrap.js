9ff2eb26ffbee50790a82febaf98ad95b899c7f3
Lê Anh Ngọc <email_github@gmail.com>
2026-02-01 16:47:29 +0700

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2916178359"
+    serviceWorkerVersion: "3037269234"
   }
 });
